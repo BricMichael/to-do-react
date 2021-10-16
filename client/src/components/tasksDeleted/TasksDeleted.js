d20f4403b2350654758791fac5408f63869dd6c9
BricMichael <bricmichael@gmail.com>
2021-10-16 14:53:34 -0400

    #7.  Estilos componente, tareas eliminadas.

--- client/src/components/tasksDeleted/TasksDeleted.js
+++ client/src/components/tasksDeleted/TasksDeleted.js
@@ -2,8 +2,22 @@ import style from './tasksDeleted.module.css';
 
 const TasksDeleted = () => {
     return (
-        <div>
-            <p>Tareas eliminadas</p>
+        <div className={style.contentTasksDeleted} >
+            <p className={style.contentTasksDeleted_title}>Lista de Tareas retiradas</p>
+
+            <div className={style.contentDescription_button}>
+                <div className={style.contentDescription}>
+                    <p className={style.contentDescription_task}><strong>Tarea:</strong>&nbsp;Comprar ropa en diciembre</p>
+
+                    <p className={style.contentDescription_created}><strong>Creada:</strong> 29/05/2021</p>
+                    <p className={style.contentDescription_deleted}><strong>Terminada:</strong> 04/06/2021</p>
+                </div>
+                <div className={style.content_button}>
+                    <button className={style.content_button_undoTask} type='button'>
+                        <i className="fas fa-trash-restore-alt"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
